549640c41466813daabde04c4d19d2bee492a17f
metishonora <laino1227@gmail.com>
2022-10-06 00:57:21 +0900

    Solve a backtracking prob

--- acmicpc/17825.cc
+++ acmicpc/17825.cc
@@ -8,30 +8,35 @@ struct Board {
 } board[33];
 
 bool same_loc(int here, int here_loc, int (&a)[4]) {
-	for (int i = 0; i < 3; i++) {
-		int there = (here + 1) % 4;
+	for (int i = 1; i <= 3; i++) {
+		int there = (here + i) % 4;
 		int there_loc = a[there];
-		if (here_loc == there_loc) return true;
+		if (here_loc != 32 && here_loc == there_loc) return true;
 	}
 	return false;
 }
 
 int dice[10];
 int max_score;
-void move(int (&a)[4], int depth) {  // 각 말의 위치, n번째 move
-	int score = board[a[0]].score + board[a[1]].score + board[a[2]].score + board[a[3]].score;
-	max_score = max(score, max_score);
+void move(int (&a)[4], int depth, int cur_score/*, string log*/) {  // 각 말의 위치, n번째 move
+	max_score = max(cur_score, max_score);
+
+	if (depth == 10) return;
+
 	int next_move = dice[depth] - 1;
 
-	int aa[4] = {a[0], a[1], a[2], a[3]};
  	for (int i = 0; i < 4; i++) {  // a b c d
+		int aa[4] = {a[0], a[1], a[2], a[3]};
 		aa[i] = board[aa[i]].starts;
 		for (int j = 0; j < next_move; j++) {
 			aa[i] = board[aa[i]].moves;
 		}
 		// 이전 말과 위치가 같아지면 가지치기
+		// string next_str = log + to_string(i);
+
 		if (same_loc(i, aa[i], a)) continue;
-		move(aa, depth+1);
+		int next_score = cur_score + board[aa[i]].score;
+		move(aa, depth+1, next_score/*, log + to_string(i)*/);
 	}
 }
 
@@ -39,7 +44,7 @@ int main() {
 	for (int i = 0; i <= 31; i++) {
 		board[i].score = i*2;
 		board[i].starts = i+1;
-		board[i+1].moves = i+1;
+		board[i].moves = i+1;
 	}
 	board[5].starts = 21;
 	board[10].starts = 24;
@@ -62,7 +67,7 @@ int main() {
 		scanf("%d", &dice[i]);
 	}
 	int arr[4] = {0,0,0,0};
-	move(arr, 0);
+	move(arr, 0, 0/*, ""*/);
 	printf("%d", max_score);
 	return 0;
 }
